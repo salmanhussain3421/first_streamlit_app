4674289e48faa555018b7cc0a2df9bfd01396403
salmanhussain3421 <127218363+salmanhussain3421@users.noreply.github.com>
2023-04-24 10:16:38 +0100

    Update streamlit_app.py
    talk about life

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthier Meal')
 streamlit.header('Main')
 streamlit.text('Jokes, you are not getting anythign served. Get Out!')
+streamlit.header('Life is great')
