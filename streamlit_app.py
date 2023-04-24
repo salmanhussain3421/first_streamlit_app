e02d1363810e72febde944f1ec68a9bc4440ed33
salmanhussain3421 <127218363+salmanhussain3421@users.noreply.github.com>
2023-04-24 10:38:20 +0100

    Update streamlit_app.py
    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,10 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.dataframe(my_fruit_list)
 streamlit.header('Giving selection')
 my_fruit_list=my_fruit_list.set_index('Fruit')
+
 # filtered to automatically add avocade and strawberries
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+#streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+#streamlit.dataframe(my_fruit_list)
 
 fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
