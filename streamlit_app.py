5465d34addac93f60a2c995e6184c85815b9ea0b
salmanhussain3421 <127218363+salmanhussain3421@users.noreply.github.com>
2023-04-24 10:29:28 +0100

    Update streamlit_app.py
    filtered updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.dataframe(my_fruit_list)
 streamlit.header('Giving selection')
 my_fruit_list=my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
-# Filtering
+# filtered to automatically add avocade and strawberries
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.dataframe(my_fruit_list)
