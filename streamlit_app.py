6f24dd20d498b717f811e74f83d6f80dd571eb39
salmanhussain3421 <127218363+salmanhussain3421@users.noreply.github.com>
2023-04-24 10:19:28 +0100

    Update streamlit_app.py
    imported pandas
    made csv object and dataframe is called

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthier Meal')
 streamlit.header('Main')
 streamlit.text('Jokes, you are not getting anythign served. Get Out!')
 streamlit.header('Life is great')
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
